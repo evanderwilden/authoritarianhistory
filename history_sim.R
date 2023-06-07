72de748ccec8b6dd7fa89a98322264638e90badd
Ethan vanderWilden <ethan.vanderwilden@wisc.edu>
2023-06-07 10:25:33 +0200

    Updating power analysis
    Re-run power analysis with more sims

--- history_sim.R
+++ history_sim.R
@@ -17,48 +17,35 @@ if (length(setdiff(pkg, rownames(installed.packages()))) > 0) {
 # Load
 lapply(pkg, library, character.only = TRUE)
 
+
 ############ 2. Load and clean the data ####
-setwd('~/Projects/memorialization')
-toy <- read.csv("history_toy.csv")
+getwd()
+setwd('C:/Users/19785/Documents/Projects/Laia_projects/')
+toy <- read.csv("stigmatization/history_toy.csv")
 attributes(toy)$subheaders <- toy[1,]
 toy <- toy[-c(1:2), -c(1:17)]
 
 
 ########## 3. Validating baseline assumptions ####
-cis <-read_sav('dec19.sav')
+cis <-read_sav('replications/oct22.sav')
 
 # from CIS, what is approval for Abascal, what is SD?
-#around 2 and 3 seem appropriate
-cis$B29_3 <- ifelse(cis$B29_3>10, NA, cis$B29_3)
-mean(cis$B29_3, na.rm = T)
-sd(cis$B29_3, na.rm = T)
+cis$abascal <- ifelse(cis$VALORALIDERES_4 >10, NA, cis$VALORALIDERES_4)
+mean(cis$abascal, na.rm = T) # 26.1
+sd(cis$abascal, na.rm = T) # 23.9
 
 
-#cis$C8 <- ifelse(cis$C8>95, NA, cis$C8)
-#cis$C8 <- ifelse(cis$C8 == 18, 1, 0)
-#mean(cis$C8, na.rm = T)
-#sd(cis$C8, na.rm = T)
-
-
-
-############# 4. Simulate Data and run a regression####
-set.seed(1234)
-
+############# 4. Write functions ####
 #function for simulating data
 simulate_data <- function(N, effects){
   #initialize data frame
   df <- data.frame(matrix(ncol = 20, nrow = N))
   names(df) <- names(toy)
   
-  #covariates
-  df$sex <- sample(c(1,2), N, replace = T)
-  df$age <- sample(c(1:6), N, replace = T)
-  df$education <- sample(c(1:6), N, replace = T)
-  df$region_1 <- sample(c(1:19), N, replace = T)
-  df$ideology_1 <- sample(c(1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 6, 6, 7), N, replace = T)
-  
   #treatment
-  df$treat <- sample(c(0:5), N, replace = T)
+  df$treat <- c(rep(0, N*0.2), rep(1, N*0.2),
+                     rep(2, N*0.2), rep(3, N*0.2),
+                     rep(4, N*0.1), rep(5, N*0.1))
   df$normal <- case_when(df$treat == 0 ~ 0,
                          df$treat == 1 ~ 1,
                          df$treat == 2 ~ 0,
@@ -98,7 +85,7 @@ simulate_data <- function(N, effects){
   
   #simulate treatment
   for (i in 1:nrow(df)){
-    if (df$treat[i] <= 3){
+    if (df$treat[i] < 4){
       df$sympathy_1[i] <- 
         rnorm(1, mean = (base + normal_tau*df$normal[i] +
                            history_tau*df$history[i] +
@@ -113,8 +100,8 @@ simulate_data <- function(N, effects){
   for (i in 1:nrow(df)){
     if(df$sympathy_1[i] < 0){
       df$sympathy_1[i] <- 0
-    } else if(df$sympathy_1[i] > 10){
-      df$sympathy_1[i] <- 10
+    } else if(df$sympathy_1[i] > 100){
+      df$sympathy_1[i] <- 100
     } else{
       df$sympathy_1[i]
     }
@@ -124,98 +111,82 @@ simulate_data <- function(N, effects){
 }
 
 # Analyze for one simulation
-effects <- c(3,    #baseline
-             -0.5, #history effect
-             0.7,  #normalization effect
-             0.4,  #interaction effect
-             -0.2, #lgbtq effect
-             2)    #sd in outcome
+effects <- c(30,    #baseline
+             -5, #history effect
+             5,  #normalization effect
+             5,  #interaction effect
+             -2, #lgbtq effect
+             23) #sd in outcome
 
 #test: simulate some data
 sim <- simulate_data(2400, effects)
 
-#extract relevant coefficients
-sympathy_coef <- as.data.frame(coef(summary(lm(sympathy_1 ~ normal + history + normal*history, data = sim))))
-names(sympathy_coef) <- c("estimate", "sd", "tval", "pval")
-sympathy_coef$variable <- row.names(sympathy_coef)
-
-ggplot(data = sympathy_coef[-c(1),]) +
-  geom_pointrange(aes(x = estimate, xmin = estimate-1.96*sd, xmax = estimate + 1.96*sd, y = variable)) +
-  geom_vline(xintercept = 0, linetype = "dashed") +
-  theme_bw() +
-  labs(title = "Regression coefficients",
-       y = "variable", 
-       x = "treatment effect")
 
 
 
 ######### 5. RUN SIMULATIONS (power calcs) ####
-
+set.seed(1234)
 #create df to store results
-simulations <- data.frame(
-  n = seq(100, 5100, 1000),
-  sig_results_4  = rep(NA, length(n)),
-  sig_results_2 = rep(NA, length(n))
+outputs <- data.frame(
+  n = seq(500, 5600, 300),
+  sig_int = rep(NA, length(n))
 )
 
 #hypothesize [LARGER] effect sizes
-effects <- c(3,    #baseline
-             -0.9, #history effect
-             1.1,  #normalization effect
-             0.8,  #interaction effect
-             -0.5, #lgbtq effect
-             2)    #sd in outcome
+effects <- c(30,    #baseline
+             -5, #history effect
+             5,  #normalization effect
+             4.8,  #interaction effect
+             -2, #lgbtq effect
+             24)   #sd in outcome
 
-#simulate results, calculate power
-for(i in 1:nrow(simulations)){
-  sig_int <- 0
-  for (j in 1:100){ #note: just running 100 simulations for now
-    sim <- simulate_data(simulations[i,1], effects)
-    sig_int <- sig_int + 
-      ifelse(coef(summary(lm(sympathy_1 ~ 
-                               normal + history + normal*history, data = sim)))[4,4] < 0.10, 1, 0)
-  }
-  simulations[i,2] <- sig_int/100
+calc_h1 <- function(data){
+  #store p-values
+  results <- 
+    c((coef(summary(lm(sympathy_1 ~ normal + history + normal*history, data = data))))[4,4])
+  
+  #sort from low to high
+  results <- sort(results)
+  
+  #add BH correction
+  ifelse(length(which(results < c(.1))) == 1, 1, 0)
 }
 
-#hypothesize [SMALLER] effect sizes
-effects <- c(3,    #baseline
-             -0.5, #history effect
-             0.7,  #normalization effect
-             0.4,  #interaction effect
-             -0.2, #lgbtq effect
-             2)    #sd in outcome
 
-#simulate results, calculate power
-for(i in 1:nrow(simulations)){
-  sig_int <- 0
-  for (j in 1:100){ #note: just running 100 simulations for now
-    sim <- simulate_data(simulations[i,1], effects)
-    sig_int <- sig_int + 
-      ifelse(coef(summary(lm(sympathy_1 ~ 
-                               normal + history + normal*history, data = sim)))[4,4] < 0.10, 1, 0)
+#simulate 100 data draws
+run_power <- function(outputs, first, last){
+  #simulate results, calculate power
+  for(i in first:last){
+    sig_int <- 0
+
+    for (j in 1:500){ #note: just running 100 simulations for now
+      sim <- simulate_data(outputs[i,1], effects)
+      sig_int <- sig_int + calc_h1(sim)
+    }
+    outputs[i,2] <- sig_int/500 #calculate proportion that was significant
   }
-  simulations[i,3] <- sig_int/100
+  
+  return(outputs)
 }
+
+outputs <- run_power(outputs, 1, 18) #run simulations
+
 
 ############# 6. Plot power calculations ####
 
-cols <- c("H1 (d = 0.4)"= "darkred",
-          "H1 (d = 0.2)"= "grey50")
-
-
-ggplot(data = simulations) +
-  geom_point(aes(x = n, y = sig_results_4, color = "H1 (d = 0.4)"), shape = 16, size = 2) + 
-  geom_smooth(aes(x = n, y = sig_results_4, color = "H1 (d = 0.4)"), se = F, method = "loess") +
+ggplot(data = outputs) +
+  #geom_point(aes(x = n, y = sig_results_4, color = "H1 (d = 0.4)"), shape = 16, size = 2) + 
+  #geom_smooth(aes(x = n, y = sig_results_4, color = "H1 (d = 0.4)"), se = F, method = "loess") +
   
-  geom_point(aes(x = n, y = sig_results_2, color = "H1 (d = 0.2)"), shape = 16, size = 2) + 
-  geom_smooth(aes(x = n, y = sig_results_2, color = "H1 (d = 0.2)"), se = F, method = "loess") +
+  geom_point(aes(x = n, y = sig_int), shape = 16, size = 2) + 
+  geom_smooth(aes(x = n, y = sig_int), se = F) +
   
-  geom_hline(yintercept = 0.8, linetype = "dashed", size = 1) +
-  scale_x_continuous(limits = c(0, 5200), breaks = seq(0, 5000, 1000))+
-  scale_color_manual(values = cols)+
+  geom_hline(yintercept = 0.9, linetype = "dashed", size = 1) +
+  scale_x_continuous(limits = c(0, 6000), breaks = seq(0, 6000, 1000))+
+  scale_y_continuous(limits = c(0, 1), breaks = c(seq(0, 0.8, 0.2), 0.9, 1))+
   theme_bw()+
-  labs(title = "Simulating Significance on Interaction Term",
+  labs(title = "Simulating Significance on Interaction Term (H1)",
+       subtitle = "Effect size of interaction at d = 0.2",
        x = "Sample Size Required (total)", y = "Power") +
   theme(legend.title = element_blank())
 
